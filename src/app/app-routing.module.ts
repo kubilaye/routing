cda4d98e35f6d22d7605d960f2331094f0f64dd3
Kubilay Erdogan <kublaios@gmail.com>
2018-03-19 18:47:12 +0300

    passing static data to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {ServerComponent} from './servers/server/server.component';
 import {UsersComponent} from './users/users.component';
 import {HomeComponent} from './home/home.component';
 import {ServersComponent} from './servers/servers.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+// import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {UserComponent} from './users/user/user.component';
 import {AuthGuard} from './auth-guard.service';
 import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,7 +20,8 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ] },
-  { path: 'not-found', component: PageNotFoundComponent },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found'} },
   { path: '**', redirectTo: 'not-found' },
 ];
 
